c1df70e013cdb010319306dbc1c5f7d262527a0f
Paulo Henrique Carvalho de Morais <paulohcarvalho9@gmail.com>
2023-11-21 17:01:31 -0300

    feat: add ResultScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -36,7 +37,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activityScreen = StartScreen(switchScreen);
+        activityScreen = const ResultsScreen();
       });
     }
   }
